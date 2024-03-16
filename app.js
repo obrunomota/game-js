08dd1631d4465747912b2f2d7624ce25d9c5c601
Bruno Mota <bruengprod@gmail.com>
2024-03-16 14:45:04 -0300

    Finalizacao do curso.

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-alert('Ola Mundo!');
+alert('Boas-vindas ao jogo do número secreto');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O numero secreto eh menor que ${chute}`);
+        }
+        else{
+            alert(`O numero secreto eh maior que ${chute}`);
+        }
+
+        tentativas++;
+    }    
+}
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Acertou! Você descobriu o numero secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
